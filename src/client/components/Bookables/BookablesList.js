d97102bc099685b2a2d8fc0afcc07d17b7c4a7e5
oumabaros <oumabaros@gmail.com>
2022-08-07 01:01:06 +0300

    First Commit

--- src/client/components/Bookables/BookablesList.js
+++ src/client/components/Bookables/BookablesList.js
@@ -44,14 +44,39 @@ export default function BookablesList(){
             </p>
         </div>
         {
-            bookable&&(
+            bookable && (
                 <div className="book-details">
                     <div className="item">
                         <div className="item-header">
                             <h2>
                                 {bookable.title}
                             </h2>
+                            <span className="controls">
+                                <label>
+                                    <input type="checkbox" checked={hasDetails} onChange={()=>setHasDetails(has=>!has)} />
+                                    Show Details
+                                </label>
+                            </span>
                         </div>
+                        <p>{bookable.notes}</p>
+                        {hasDetails && (
+                            <div className="item-details">
+                                <h3>Availability</h3>
+                                <div className="bookable-availability">
+                                    <ul>
+                                        {
+                                            bookable.days.sort().map(d=><li key={d}>{ bookable.days[d]}</li>)
+                                            
+                                        }
+                                    </ul>
+                                    <ul>
+                                        {
+                                            bookable.sessions.map(s=><li key={s}>{ bookable.sessions[s]}</li>)
+                                        }
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             )
