ed8d9fde3c75b9bae3c2378c3f343f7a0022485c
oumabaros <oumabaros@gmail.com>
2022-08-21 09:39:36 +0300

    first commit

--- src/client/components/Users/UserPicker.js
+++ src/client/components/Users/UserPicker.js
@@ -2,7 +2,7 @@ import React,{useState, useEffect} from "react";
 import Spinner from "../UI/Spinner";
 import getData from "../../utils/api";
 
-export default function UserPicker () {
+export default function UserPicker ({user,setUser}) {
     const [users, setUsers] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     //const [error, setError] = useState(null);
@@ -11,13 +11,14 @@ export default function UserPicker () {
         getData("http://localhost:3001/users")
             .then(data => {
                 setUsers(data);
+                setUser(data[0]);
                 setIsLoading(false);
             })
             .catch(error => {
                 //setError(error);
                 setIsLoading(false);
             });
-        }, []); 
+        }, [setUser]); 
 
         /*if (error) {
             return <p>{error.message}</p>
@@ -27,12 +28,17 @@ export default function UserPicker () {
             return <p><Spinner/> Loading users...</p>
         }
         
+        function handleSelect(e){
+            const selectedID = parseInt(e.target.value, 10);
+            const selectedUser = users.find(u => u.id === selectedID);
+            setUser(selectedUser)
+        }
 
         return (
-            <select>
+            <select className="user-picker" onChange={handleSelect} value={user?.id}>
                 {
                     users.map((u)=>(
-                        <option key={u.id}>
+                        <option key={u.id} value={u.id}>
                             {u.name}
                         </option>
                     ))
